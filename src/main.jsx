ea37f38ee176384d9f2e206cce8a0fe09e4eaa14
pa4080 <spas.z.spasov@gmail.com>
2023-01-21 14:29:21 +0200

    Create a React component <Game /> and render it inside root

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <h1>Board</h1>
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
 /**
  * Get the dedicated in the HTML file element (with id="root")
  * create react Root and render some content.
  */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>"Hello World!"</h1>);
+root.render(<Game />);
