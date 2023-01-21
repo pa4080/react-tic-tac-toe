5ed26eae434da10334f9ba656fad7a015e6ef74c
pa4080 <spas.z.spasov@gmail.com>
2023-01-21 14:22:37 +0200

    Get the root element from the HTML DOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+
+/**
+ * Get the dedicated in the HTML file element (with id="root")
+ * create react Root and render some content.
+ */
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1>"Hello World!"</h1>);
