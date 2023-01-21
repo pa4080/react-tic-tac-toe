0f072e05032aae63a2a28423eaebe1f8e9a8b03b
pa4080 <spas.z.spasov@gmail.com>
2023-01-21 14:40:16 +0200

    Create <Square /> component and render

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+class Square extends React.Component {
+  render() {
+    return <button className="square">{/* TODO */}</button>;
+  }
+}
+
 class Board extends React.Component {
   renderSquare(i) {
-    return "<Square />";
+    return <Square />;
   }
 
   render() {
