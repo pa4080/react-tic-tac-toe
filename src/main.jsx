5a982932fb3500cedcec419d940546c97d175b1d
pa4080 <spas.z.spasov@gmail.com>
2023-01-21 19:19:25 +0200

    Function Components: rewrite <Square />

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClickBoard()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={() => props.onClickBoard()}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
