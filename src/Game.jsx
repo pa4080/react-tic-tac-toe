0f515f01be71c38fb43c650b60284ca276cab179
pa4080 <spas.z.spasov@gmail.com>
2023-01-21 21:54:33 +0200

    Import <Game/> as Module in main.jsx; mitigate a minor bug with Vite dev server an live reload

---
+++ src/Game.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClickBoard}>
+      {props.value}
+    </button>
+  );
+}
+class Board extends React.Component {
+  constructor(props) {
+    super(props); // Call the parent's class constructor
+
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
+  handleClick(i) {
+    // https://reactjs.org/tutorial/tutorial.html#why-immutability-is-important
+    // const squares = this.state.squares.slice();
+    const squares = [...this.state.squares];
+    squares[i] = "X";
+    this.setState({ squares: squares });
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        // onClickBoard() doesn't use any parameters, so we
+        // should not pass any arguments when call it!
+        onClickBoard={() => {
+          this.handleClick(i);
+        }}
+      />
+    );
+  }
+
+  render() {
+    const status = "Next player: X";
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Game;
--- src/main.jsx
+++ src/main.jsx
@@ -1,86 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-
-function Square(props) {
-  return (
-    <button className="square" onClick={props.onClickBoard}>
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  constructor(props) {
-    super(props); // Call the parent's class constructor
-
-    this.state = {
-      squares: Array(9).fill(null)
-    };
-  }
-
-  handleClick(i) {
-    // https://reactjs.org/tutorial/tutorial.html#why-immutability-is-important
-    // const squares = this.state.squares.slice();
-    const squares = [...this.state.squares];
-    squares[i] = "X";
-    this.setState({ squares: squares });
-  }
-
-  renderSquare(i) {
-    return (
-      <Square
-        value={this.state.squares[i]}
-        // onClickBoard() doesn't use any parameters, so we
-        // should not pass any arguments when call it!
-        onClickBoard={() => {
-          this.handleClick(i);
-        }}
-      />
-    );
-  }
-
-  render() {
-    const status = "Next player: X";
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
+import Game from "./Game";
 
 /**
  * Get the dedicated in the HTML file element (with id="root")
@@ -93,3 +14,9 @@ root.render(
     <Game />
   </React.StrictMode>
 );
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Game />
+//   </React.StrictMode>
+// );
