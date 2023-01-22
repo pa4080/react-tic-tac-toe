c3e9201e949148436f4c010936bb607242c1d900
pa4080 <spas.z.spasov@gmail.com>
2023-01-22 13:37:19 +0200

    Lifting State Up: Game <  Board (become controlled component)

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useRef } from "react";
 import Board from "./components/Board";
+import { calculateWinner } from "./helpers/Calculate";
 
 // Disable the hot reload, https://stackoverflow.com/a/74817610/6543935
 // if (import.meta.hot) import.meta.hot.accept(() => import.meta.hot.invalidate());
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props); // Call the parent's class constructor
+
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null)
+        }
+      ],
+      xIsNext: true
+    };
+  }
+
+  handleClick(i) {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    // https://reactjs.org/tutorial/tutorial.html#why-immutability-is-important
+    // const squares = this.props.squares.slice();
+    const squares = [...current.squares];
+
+    // Ignoring a click if someone has won the game or if a Square is already filled
+    if (calculateWinner(squares) || squares[i]) return;
+
+    squares[i] = this.state.xIsNext ? "X" : "O";
+
+    this.setState({
+      history: history.concat([{ squares: squares }]),
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+
+    let status;
+
+    if (winner) {
+      status = "Winner is: " + winner;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClickGame={(i) => {
+              this.handleClick(i);
+            }}
+          />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,60 +1,19 @@
 import React from "react";
 import Square from "./Square";
-import { calculateWinner } from "../helpers/Calculate";
 
 class Board extends React.Component {
-  constructor(props) {
-    super(props); // Call the parent's class constructor
-
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-      winner: false
-    };
-  }
-
-  handleClick(i) {
-    // https://reactjs.org/tutorial/tutorial.html#why-immutability-is-important
-    // const squares = this.state.squares.slice();
-    const squares = [...this.state.squares];
-
-    // Ignoring a click if someone has won the game or if a Square is already filled
-    if (calculateWinner(squares) || squares[i]) return;
-
-    squares[i] = this.state.xIsNext ? "X" : "O";
-
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-      winner: this.state.winner
-    });
-  }
-
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        // onClickBoard() doesn't use any parameters, so we
-        // should not pass any arguments when call it!
-        onClickBoard={() => {
-          this.handleClick(i);
-        }}
+        value={this.props.squares[i]}
+        onClickBoard={() => this.props.onClickGame(i)}
       />
     );
   }
 
   render() {
-    let status;
-    const winner = calculateWinner(this.state.squares);
-    if (winner) {
-      status = "Winner is: " + winner;
-    } else {
-      status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
