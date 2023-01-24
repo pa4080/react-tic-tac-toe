d7450a9aef1f029aef15ef149a5da1107f994fd5
pa4080 <spas.z.spasov@gmail.com>
2023-01-24 12:23:12 +0200

    Function Components: rewrite <Board />

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,40 +1,36 @@
 import React from "react";
 import Square from "./Square";
 
-class Board extends React.Component {
-  renderSquare(i) {
+function Board(props) {
+  const renderSquare = (i) => {
     return (
       <Square
-        value={this.props.squares[i]}
-        onClickBoard={() => this.props.onClickGame(i)}
-        highlight={
-          Array.isArray(this.props.lines) && this.props.lines.includes(i)
-        }
+        value={props.squares[i]}
+        onClickBoard={() => props.onClickGame(i)}
+        highlight={Array.isArray(props.lines) && props.lines.includes(i)}
       />
     );
-  }
+  };
 
-  render() {
-    let index = 0;
+  let index = 0;
 
-    return (
-      <div className="board rounded-lg shadow-xl">
-        <table className="board-table rounded-lg bg-white">
-          <tbody>
-            {[0, 1, 2].map((y) => {
-              return (
-                <tr key={y}>
-                  {[0, 1, 2].map((x) => {
-                    return <td key={x}>{this.renderSquare(index++)}</td>;
-                  })}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+  return (
+    <div className="board rounded-lg shadow-xl">
+      <table className="board-table rounded-lg bg-white">
+        <tbody>
+          {[0, 1, 2].map((y) => {
+            return (
+              <tr key={y}>
+                {[0, 1, 2].map((x) => {
+                  return <td key={x}>{renderSquare(index++)}</td>;
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Board;
