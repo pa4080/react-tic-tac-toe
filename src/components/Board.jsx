f19677d8ac623ce0f96e1e1becb85c44b7e05519
pa4080 <spas.z.spasov@gmail.com>
2023-01-24 12:03:42 +0200

    Use two Loops to Draw the Board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -15,25 +15,21 @@ class Board extends React.Component {
   }
 
   render() {
+    let index = 0;
+
     return (
       <div className="board rounded-lg shadow-xl">
         <table className="board-table rounded-lg bg-white">
           <tbody>
-            <tr>
-              <td>{this.renderSquare(0)}</td>
-              <td>{this.renderSquare(1)}</td>
-              <td>{this.renderSquare(2)}</td>
-            </tr>
-            <tr>
-              <td>{this.renderSquare(3)}</td>
-              <td>{this.renderSquare(4)}</td>
-              <td>{this.renderSquare(5)}</td>
-            </tr>
-            <tr>
-              <td>{this.renderSquare(6)}</td>
-              <td>{this.renderSquare(7)}</td>
-              <td>{this.renderSquare(8)}</td>
-            </tr>
+            {[0, 1, 2].map((y) => {
+              return (
+                <tr key={y}>
+                  {[0, 1, 2].map((x) => {
+                    return <td key={x}>{this.renderSquare(index++)}</td>;
+                  })}
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
