256a79e3aa18b88a3ede1678f01ad227350befb2
pa4080 <spas.z.spasov@gmail.com>
2023-01-25 09:03:42 +0200

    Function Components: rewrite <GameTicTacToe />

--- src/GameTicTacToe.jsx
+++ src/GameTicTacToe.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./components/Board";
 import PlayerHeart from "./components/PlayerHeart";
 import PlayerStar from "./components/PlayerStar";
@@ -9,40 +9,52 @@ import { calculateWinner } from "./helpers/Calculate";
 // Disable the hot reload, https://stackoverflow.com/a/74817610/6543935
 // if (import.meta.hot) import.meta.hot.accept(() => import.meta.hot.invalidate());
 
-class GameTicTacToe extends React.Component {
-  constructor(props) {
-    super(props); // Call the parent's class constructor
+function GameTicTacToe() {
+  const [gameHistory, setGameHistory] = useState([
+    {
+      squares: Array(9).fill(null),
+      x: null,
+      y: null,
+      xIsNext: true,
+      number: 0
+    }
+  ]);
 
-    this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null),
-          x: null,
-          y: null,
-          xIsNext: true,
-          number: 0
-        }
-      ],
-      stepNumber: 0,
-      reverseHistDisplay: false,
-      resetGame: false
-    };
+  const [stepNumber, setStepNumber] = useState(0);
+  const [reverseHistory, setReverseHistory] = useState(false);
+  const [resetGame, setResetGame] = useState(false);
+
+  function handleResetGame() {
+    setGameHistory([
+      {
+        squares: Array(9).fill(null),
+        x: null,
+        y: null,
+        xIsNext: true,
+        number: 0
+      }
+    ]);
+
+    setStepNumber(0);
+    setResetGame(true);
+
+    setTimeout(() => {
+      setResetGame(false);
+    }, 100);
   }
 
-  handleClick(i, x, y) {
-    // console.log(i, x + 1, y + 1);
-
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const squares = [...current.squares]; // const squares = this.props.squares.slice();
+  function handleClick(i, x, y) {
+    const history = gameHistory.slice(0, stepNumber + 1); // reset the game from the history, whe continue
+    const current = history[history.length - 1]; // get the current game from the history
+    const squares = [...current.squares]; // const squares = history.squares.slice();
 
     // Ignoring a click if someone has won the game or if a Square is already filled
     if (calculateWinner(squares).winner || squares[i]) return;
 
     squares[i] = current.xIsNext ? "X" : "O";
 
-    this.setState({
-      history: history.concat([
+    setGameHistory(
+      history.concat([
         {
           squares: squares,
           x: x,
@@ -50,134 +62,103 @@ class GameTicTacToe extends React.Component {
           xIsNext: !current.xIsNext,
           number: history.length
         }
-      ]),
-      stepNumber: history.length
-    });
+      ])
+    );
+
+    setStepNumber(history.length);
   }
 
-  jumpTo(step) {
-    // State Updates are Merged: React will update only the properties mentioned in
-    //                           setState method leaving the remaining state as is.
-    // https://reactjs.org/docs/state-and-lifecycle.html#state-updates-are-merged
-    this.setState({
-      stepNumber: step
-    });
+  function jumpTo(step) {
+    setStepNumber(step);
   }
 
-  getPlayer(xIsNext) {
+  function getPlayer(xIsNext) {
     return xIsNext ? <PlayerStar /> : <PlayerHeart />;
   }
 
-  handleSwitchOrder(order) {
-    this.setState({ reverseHistDisplay: order });
+  function handleSwitchOrder(order) {
+    setReverseHistory(order);
   }
 
-  handleResetGame() {
-    this.setState({
-      history: [
-        {
-          squares: Array(9).fill(null),
-          x: null,
-          y: null,
-          xIsNext: true,
-          number: 0
-        }
-      ],
-      stepNumber: 0,
-      reverseHistDisplay: false,
-      resetGame: true
-    });
-    setTimeout(() => {
-      this.setState({ resetGame: false });
-    }, 100);
-  }
+  /**
+   * Render the output
+   */
+  const history = [...gameHistory];
+  const current = history[stepNumber];
+  const { winner, lines } = calculateWinner(current.squares);
 
-  render() {
-    const history = [...this.state.history];
-    const current = history[this.state.stepNumber];
-    const { winner, lines } = calculateWinner(current.squares);
-
-    const movies = history.map((step, movie, arr) => {
-      const description = movie ? (
-        <span className="items-center flex w-full justify-between pr-2">
-          <span>
-            {movie === arr.length - 1 ? "Last move" : "Go to move"}: #{movie}
-          </span>
-          <span>
-            [x:{step.x}, y:{step.y}]
-          </span>
+  const movies = history.map((step, movie, arr) => {
+    const description = movie ? (
+      <span className="items-center flex w-full justify-between pr-2">
+        <span>
+          {movie === arr.length - 1 ? "Last move" : "Go to move"}: #{movie}
         </span>
-      ) : (
-        <span>Go to game start</span>
-      );
-
-      return (
-        // It’s strongly recommended that you assign
-        // proper keys whenever you build dynamic lists.
-        // Keys do not need to be globally unique.
-        <li
-          key={movie}
-          className={`py-2 px-2 text-lg${
-            current.number === step.number ? " bg-orange-100" : ""
-          }`}
-        >
-          <button
-            aria-label="History jump"
-            onClick={() => this.jumpTo(movie)}
-            className="items-center flex w-full justify-between"
-          >
-            {description}
-            {movie ? this.getPlayer(!step.xIsNext) : ""}
-          </button>
-        </li>
-      );
-    });
+        <span>
+          [x:{step.x}, y:{step.y}]
+        </span>
+      </span>
+    ) : (
+      <span>Go to game start</span>
+    );
 
     return (
-      <div className="game pt-10">
-        <h1 className="text-3xl font-bold underline mb-10 text-gray-700">
-          Tic Tac Toe
-        </h1>
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            onClickGame={(i, x, y) => {
-              this.handleClick(i, x, y);
-            }}
-            lines={lines}
-          />
-        </div>
-
-        <div className="mt-6">
-          <Status winner={winner} xIsNext={current.xIsNext} />
-        </div>
-
+      <li
+        key={movie}
+        className={`py-2 px-2 text-lg${
+          current.number === step.number ? " bg-orange-100" : ""
+        }`}
+      >
         <button
-          aria-label="Restart"
-          onClick={() => this.handleResetGame()}
-          className={`py-3 px-6 text-center fit-content mt-6 rounded-lg font-bold text-lg text-white transition duration-100 ${
-            this.state.resetGame
-              ? "bg-red-400 shadow-sm"
-              : "bg-red-500 shadow-md"
-          }`}
+          aria-label="History jump"
+          onClick={() => jumpTo(movie)}
+          className="items-center flex w-full justify-between"
         >
-          Restart Game
+          {description}
+          {movie ? getPlayer(!step.xIsNext) : ""}
         </button>
-        <div className="game-info mt-10 pb-10">
-          <div>
-            <SwitchToggle switch={(order) => this.handleSwitchOrder(order)} />
-          </div>
+      </li>
+    );
+  });
 
-          {/* <div>
-            <TestListBox />
-          </div> */}
-          <div className="relative w-full cursor-default rounded-lg bg-white py-2 text-left shadow-md focus:outline-none">
-            <ol>{this.state.reverseHistDisplay ? movies.reverse() : movies}</ol>
-          </div>
+  return (
+    <div className="game pt-10">
+      <h1 className="text-3xl font-bold underline mb-10 text-gray-700">
+        Tic Tac Toe
+      </h1>
+      <div className="game-board">
+        <Board
+          squares={current.squares}
+          onClickGame={(i, x, y) => {
+            handleClick(i, x, y);
+          }}
+          lines={lines}
+        />
+      </div>
+
+      <div className="mt-6">
+        <Status winner={winner} xIsNext={current.xIsNext} />
+      </div>
+
+      <button
+        aria-label="Restart Game"
+        onClick={() => handleResetGame()}
+        className={`py-3 px-6 text-center fit-content mt-6 rounded-lg font-bold text-lg text-white transition duration-100 ${
+          resetGame ? "bg-red-400 shadow-sm" : "bg-red-500 shadow-md"
+        }`}
+      >
+        Restart Game
+      </button>
+      <div className="game-info mt-10 pb-10">
+        <div>
+          <SwitchToggle switch={(order) => handleSwitchOrder(order)} />
+        </div>
+
+        <div className="relative w-full cursor-default rounded-lg bg-white py-2 text-left shadow-md focus:outline-none">
+          <ol>{reverseHistory ? movies.reverse() : movies}</ol>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default GameTicTacToe;
